183aaa322cd714b702b14a12c2bf200f8f93e2f6
Никита Гайко <nik.gayko@gmail.com>
2019-02-03 13:12:43 +0300

    init commit

---
+++ piggy-bank/piggy-bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piggy-bank
+//
+//  Created by Никита Гайко on 03/02/2019.
+//  Copyright © 2019 Nikita Gayko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
